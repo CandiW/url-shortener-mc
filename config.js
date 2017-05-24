3c93e0ef8f37a25ea148b1739248437c9d90eefe
CandiW <candisw06@gmail.com>
2017-05-24 06:14:28 -0700

    updates

---
+++ config.js
@@ -0,0 +1,8 @@
+let config = {};
+
+config.db = {};
+
+config.db.host = 'CandiW:jesus123@ds151941.mlab.com:51941/candiw-url-shortener';
+config.db.name = 'candiw-url-shortener';
+
+module.exports = config;
